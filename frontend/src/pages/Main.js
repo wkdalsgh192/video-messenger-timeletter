e4bcc4bd21306caaf75fc9da256c82343a9f1ebb
이대헌 <eogjs4818@naver.com>
2021-04-29 18:40:35 +0900

    feat : redux 환경설정, 그룹리스트 페이지, 그룹생성 페이지

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+const { logIn } = require("../_actions/user");
+const userSlice = require("../_reducers/user");
 
 function Main() {
-    return (
-        <div>
-            메인페이지
-        </div>
-    )
+  const user = useSelector((state) => state.user);
+  // const { list } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+
+  const onClick = useCallback(() => {
+    dispatch(
+      logIn({
+        id: "zerocho",
+        password: "비밀번호",
+      })
+    );
+    // dispatch(logIn());
+  }, []);
+
+  const onLogout = useCallback(() => {
+    dispatch(userSlice.actions.logOut());
+  }, []);
+
+  return (
+    <div>
+      {user.isLoggingIn ? (
+        <div>로그인 중</div>
+      ) : user.data ? (
+        <div>{user.data.nickname}</div>
+      ) : (
+        "로그인 해주세요."
+      )}
+      {!user.data ? (
+        <button onClick={onClick}>로그인</button>
+      ) : (
+        <button onClick={onLogout}>로그아웃</button>
+      )}
+    </div>
+  );
 }
 
-export default Main
+export default Main;
--- frontend/src/pages/UserPage/Signup.js
+++ frontend/src/pages/UserPage/Signup.js
@@ -9,9 +9,8 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
-
 import "./Login.css";
-import "./Signup.css"
+import "./Signup.css";
 
 // import { Link } from "react-router-dom";
 
