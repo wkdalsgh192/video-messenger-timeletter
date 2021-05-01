444422208071bc2078b349cfb019004a3b39094e
장민호 <paranpu@naver.com>
2021-05-01 15:33:52 +0900

    feat(devops): modify jenkins file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 import {Provider} from 'react-redux';
-import {store} from './store';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}> 
