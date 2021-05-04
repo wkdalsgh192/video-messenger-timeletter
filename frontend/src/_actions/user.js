40689bd541cd794539b6e364944dca42b185cb21
anseik <kma9271@naver.com>
2021-05-04 18:39:03 +0900

    bug fix

--- frontend/src/_actions/user.js
+++ frontend/src/_actions/user.js
@@ -6,7 +6,7 @@ const delay = (time, value) => new Promise((resolve, reject) => {
   }, time);
 });
 
-exports.logIn = createAsyncThunk('user/logIn', async (data, thunkAPI) => {
+module.exports.logIn = createAsyncThunk('user/logIn', async (data, thunkAPI) => {
   // throw new Error('비밀번호가 틀렸습니다.');
   return await delay(500,{
     userId: 1,
