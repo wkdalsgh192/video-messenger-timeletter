3157fb0e6d014b4a48956f6570a9bb79c2314b11
anseik <kma9271@naver.com>
2021-05-06 00:00:26 +0900

    til

--- frontend/src/_actions/user.js
+++ frontend/src/_actions/user.js
@@ -6,10 +6,10 @@ const delay = (time, value) => new Promise((resolve, reject) => {
   }, time);
 });
 
-// module.exports.logIn = createAsyncThunk('user/logIn', async (data, thunkAPI) => {
-//   // throw new Error('비밀번호가 틀렸습니다.');
-//   return await delay(500,{
-//     userId: 1,
-//     nickname: 'zerocho'
-//   });
-// });
+exports.logIn = createAsyncThunk('user/logIn', async (data, thunkAPI) => {
+  // throw new Error('비밀번호가 틀렸습니다.');
+  return await delay(500,{
+    userId: 1,
+    nickname: 'zerocho'
+  });
+});
