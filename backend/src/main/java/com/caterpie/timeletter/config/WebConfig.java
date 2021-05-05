cbf614e34f47f00f60bd82db6bec3e64b2bebaf8
DESKTOP-KVCQHCD\multicampus <hyunsub2167@gmail.com>
2021-05-05 16:43:31 +0900

    fix: 🐛 cors origin 오류 정정

--- backend/src/main/java/com/caterpie/timeletter/config/WebConfig.java
+++ backend/src/main/java/com/caterpie/timeletter/config/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
-        .allowedOrigins("https://k4d105.p.ssafy.io");
+	  registry.addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+      .allowedHeaders("*");
   }
 }
