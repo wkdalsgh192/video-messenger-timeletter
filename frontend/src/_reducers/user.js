8aefc547bf8548c95bca24a7786f21ddde836c97
이혜진 <lhj6586@naver.com>
2021-05-05 03:49:18 +0000

    Update user.js

--- frontend/src/_reducers/user.js
+++ frontend/src/_reducers/user.js
@@ -1,31 +1,31 @@
-const { createSlice } = require('@reduxjs/toolkit');
-const { logIn } = require('../_actions/user');
+// const { createSlice } = require('@reduxjs/toolkit');
+// const { logIn } = require('../_actions/user');
 
-const initialState = {
-  isLoggingIn: false,
-  data: null,
-};
+// const initialState = {
+//   isLoggingIn: false,
+//   data: null,
+// };
 
-const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    logOut(state, action) {
-      state.data = null;
-    }
-  },
-  extraReducers: (builder) => builder
-    .addCase(logIn.pending, (state, action) => {
-      state.data = null;
-      state.isLoggingIn = true;
-    })
-    .addCase(logIn.fulfilled, (state, action) => {
-      state.data = action.payload;
-      state.isLoggingIn = false;
-    })
-    .addCase(logIn.rejected, (state, action) => {
-      state.error = action.payload;
-    })
-})
+// const userSlice = createSlice({
+//   name: 'user',
+//   initialState,
+//   reducers: {
+//     logOut(state, action) {
+//       state.data = null;
+//     }
+//   },
+//   extraReducers: (builder) => builder
+//     .addCase(logIn.pending, (state, action) => {
+//       state.data = null;
+//       state.isLoggingIn = true;
+//     })
+//     .addCase(logIn.fulfilled, (state, action) => {
+//       state.data = action.payload;
+//       state.isLoggingIn = false;
+//     })
+//     .addCase(logIn.rejected, (state, action) => {
+//       state.error = action.payload;
+//     })
+// })
 
-module.exports = userSlice;
+// module.exports = userSlice;
