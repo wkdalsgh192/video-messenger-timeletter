5836ba564a0f9cd6c265966981012a44506dc9b2
이대헌 <eogjs4818@naver.com>
2021-05-07 00:18:39 +0900

    style : main페이지 css

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -14,7 +14,7 @@ import img1 from "./UserPage/images/종이비행기.gif";
 // import {FaArrowDown} from 'react-icons/fa'
 import { IoIosArrowDown } from "react-icons/all";
 // import Grid from '@material-ui/core/Grid';
-
+import { Link } from "react-router-dom";
 import "./main.css";
 const { logIn } = require("../_actions/user");
 const userSlice = require("../_reducers/user");
@@ -29,9 +29,10 @@ function Main() {
     <div className="main-wrap">
       <div className='main-html'>
         <div className="section section-1">
-          {/* <div><p>로그인하러가기</p>
-            <p>캡슐 찾으러가기</p>
-          </div> */}
+          <div>
+            <Link to="/login"><div style={{color:"white", paddingTop:"200px", fontSize:"3rem"}}>로그인하러가기</div></Link>
+            <div style={{color:"white", fontSize:"3rem"}} onClick={()=>window.scrollTo({top:"680",behavior:"smooth"})}>캡슐 찾으러가기</div>
+          </div>
           
           <div className="arrowstyle" onClick={scrolling}>
             <div>사이트 이용법 보기</div>
