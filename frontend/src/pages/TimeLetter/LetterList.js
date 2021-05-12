31143680a69715e68f10486bab9b2f7f8d740d12
anseik <kma9271@naver.com>
2021-05-13 00:31:49 +0900

    feat: letter list

--- frontend/src/pages/TimeLetter/LetterList.js
+++ frontend/src/pages/TimeLetter/LetterList.js
@@ -1,153 +1,114 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import { 
+import React, { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
   Container,
-  Typography,
-  Card,
-  CardMedia,
-  CardContent,
-} from '@material-ui/core';
-import { useHistory } from 'react-router-dom';
-import Carousel from 'react-material-ui-carousel'
-import openLetter from "../../static/images/openLetter2.png"
-import notOpenLetter from "../../static/images/notOpenLetter.png"
-import MapList from "../../components/timeletter/MapList"
+  Tabs, 
+  Tab,
+  Link,
+  // Card,
+  // Grid,
+  // CardContent,
+  // Typography,
+  Chip,
+} from '@material-ui/core'
+import FaceIcon from '@material-ui/icons/Face'
+import yellow from '@material-ui/core/colors/yellow';
+import bgImage from 'pages/images/sky2.jpg'
+// import closeletter from 'components/mypage/images/close.png'
+import './css/letter.scss'
+import './css/LetterList.css'
 
+
+// 스타일
 const useStyles = makeStyles((theme) => ({
   container: {
-    marginBottom: '80px',
-  },
-  title: {
-    marginTop: '16px',
-  },
-  paper: {
-    marginTop: theme.spacing(2),
-    display: 'flex',
-    flexDirection: 'column',
-    backgroundColor: '#e8eaf6',
-    padding: theme.spacing(0, 2),
-  },
-  category: {
-    marginBottom: '10px',
-  },
-  media: {
-    height: 200,
-    width: '70%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    backgroundImage: `url(${bgImage})`,
+    height: '100%',
+    width: '100%',
+    paddingTop: '70px',
+    paddingBottom: '700px',
+    color: '#ffcc80'
   },
 }))
 
-// 함수형 컴포넌트
+const yw = yellow[900]
+
+// 컴포넌트
 const LetterList = () => {
   const classes = useStyles()
-  const history = useHistory()
-  const openCapsules = [
-    {
-      id: 1,
-      title: 'letter1',
-    },
-    {
-      id: 2,
-      title: 'letter2',
-    },
-    {
-      id: 3,
-      title: 'letter3'
-    },
-    {
-      id: 4,
-      title: 'letter4'
-    },
-    {
-      id: 5,
-      title: 'letter5'
-    },
-    {
-      id: 6,
-      title: 'letter6'
-    },
-  ]
 
-  const notOpenCapsules = [
-    {
-      id: 7,
-      title: 'letter7',
-      lat: 33.450705,
-      lng: 126.570677,
-    },
-    {
-      id: 8,
-      title: 'letter8',
-      lat: 33.450105,
-      lng: 126.570223,
-    },
-    {
-      id: 9,
-      title: 'letter9',
-      lat: 33.450465,
-      lng: 126.570452,
-    },
-    {
-      id: 10,
-      title: 'letter10',
-      lat: 33.450103,
-      lng: 126.570546,
-    },
-    {
-      id: 11,
-      title: 'letter11',
-      lat: 33.450456,
-      lng: 126.570987,
-    },
-    {
-      id: 12,
-      title: 'letter12',
-      lat: 33.450453,
-      lng: 126.570741,
-    },
-  ]
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue)
+    setValue(newValue);
+  };
 
   return (
-    <Container className={classes.container} style={{marginTop:"50px"}} maxWidth="xs">
-      <Typography className={classes.title} variant="h6">레터 조회</Typography>
-      <div className={classes.paper}>
+    <Container className={classes.container} maxWidth="xs">
+        {/* 오픈, 비오픈 구분 탭 */}
+        <Tabs
+          value={value}
+          indicatorColor="primary"
+          onChange={handleChange}
+          aria-label="disabled tabs example"
+          style={{marginBottom:"15px", color:"bisque"}}
+        >
+          <Tab label="오픈된 레터" />
+          <Tab label="비오픈된 레터" />
+        </Tabs>
 
-        {/* 오픈 레터 */}
-        <div className={classes.category}>
-          <Typography className={classes.title} variant="subtitle1">오픈 레터</Typography>
-          <Carousel autoPlay={false}>
-            {openCapsules.map(openCapsule => (
-              <Card key={openCapsule.id} onClick={() => history.push('detail/' + openCapsule.id)}>
-                <CardMedia
-                  className={classes.media}
-                  image={openLetter}
-                />
-                <CardContent style={{textAlign: 'center'}}>{openCapsule.title}</CardContent>
-              </Card>
-            ))}
-          </Carousel>
+
+        {/* 레터 목록 */}
+        <div className="trashnone">
+          <div class="night2">
+            <span class="moon"></span>
+            <span class="spot1"></span>
+            <span class="spot2"></span>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+            <Link to="/letter/detail">
+              <div className="lettercontent2">
+                <div style={{fontSize:"20px", color: '#fff'}}>레터 이름</div>
+                <div style={{fontSize:"35px", color: '#fff'}}> 오픈시각 d-day</div>
+                <div className="lettercontent" style={{marginTop:"10px", marginBottom:"10px"}}>
+                  <Chip variant="outlined" size="middle" icon={<FaceIcon />} label="From.조현섭" color={yw} />
+                </div>
+              </div>
+            </Link>
+          </div>          
         </div>
 
-        {/* 비오픈 레터 */}
-        <div className={classes.category}>
-          <Typography className={classes.title} variant="subtitle1">비오픈 레터</Typography>
-          <MapList capsules={notOpenCapsules} />
-          <Carousel autoPlay={false}>
-            {notOpenCapsules.map(notOpenCapsule => (
-              <Card key={notOpenCapsule.id} onClick={() => history.push('detail/' + notOpenCapsule.id)}>
-                <CardMedia
-                  className={classes.media}
-                  image={notOpenLetter}
-                />
-                <CardContent style={{textAlign: 'center'}}>{notOpenCapsule.title}</CardContent>
-              </Card>
-            ))}
-          </Carousel>
-        </div>
-      </div>
+        {/* 레터 목록 */}
+        {/* <Link to="/letter/detail">
+          <Card className="">
+            <Grid container alignItems="center" className="letterbox">
+              <Grid item className="lettercontent">
+                <CardContent>
+                  <Typography variant="subtitle1" color="textSecondary">
+                    레터 이름
+                  </Typography>
+                  <Typography component="h5" variant="h5">
+                    오픈시각 d-day
+                  </Typography>
+                </CardContent>
+              </Grid>
+              <Grid item style={{marginRight:"15px"}} >
+                <img src={closeletter} style={{ height: "70px", width: "70px" }} className="tossing" />
+              </Grid>
+              <Grid item className="lettercontent" style={{marginLeft:"10px", marginBottom:"10px"}}>
+                <Chip variant="outlined" size="small" icon={<FaceIcon />} label="From.조현섭" color="secondary" />
+              </Grid>
+            </Grid>
+          </Card>
+        </Link> */}
     </Container>
-  )
-}
+  );
+};
 
-export default LetterList
+export default LetterList;
