b6fcd5957e9e995fcf387657b2706b409f4470ad
장민호 <paranpu@naver.com>
2021-05-18 15:31:17 +0900

    feat(alarm): add send message api

--- backend/src/main/java/com/caterpie/timeletter/util/AlarmScheduler.java
+++ backend/src/main/java/com/caterpie/timeletter/util/AlarmScheduler.java
@@ -25,9 +25,9 @@ public class AlarmScheduler {
 	@Autowired
 	private AlarmRepository alarmRepository;
 
-	@Scheduled(cron = "0 1 0 * * *")	//매일 00시01분 실행
+//	@Scheduled(cron = "0 1 0 * * *")	//매일 00시01분 실행
 //	@Scheduled(cron = "0/10 * * * * *")	//10초에 한번씩 실행
-//	@Scheduled(cron = "0 0/10 * * * *")	//1분에 한번씩 실행
+	@Scheduled(cron = "0 0/1 * * * *") //1분에 한번씩 실행
     public void cronJob() {
     	logger.info("scheduled");
     	
@@ -35,16 +35,22 @@ public class AlarmScheduler {
     	List<Map<Alarm, Object>> alarms = alarmRepository.findClosedLetters();	
     	
     	//알람테이블에 하나씩 셋팅
-    	for(int i=0, l=alarms.size(); i<l; i++) {		
+    	for(int i=0, l=alarms.size();i<l; i++) {		
     		int userId = (int) alarms.get(i).get("user_id");
     		int letterId = (int) alarms.get(i).get("letter_id");
+    		alarmRepository.updateLetter(letterId);	//비공개->공개 처리 
     		alarmRepository.insertAlarm(userId, letterId);	
-    		alarmRepository.updateLetter(letterId);	//비공개->공개 처리
     	}
     	
     	// 알람 LetterId 가지고 Letter랑 Target 가지고 와서 Iterate하면서 문자 발송.
-    	
-    	//문자 보내기(letter_code)
+    	//문자 보내기(letter_code, 휴대폰 번호)
+    	alarms.stream().forEach(a -> {
+    		String letterCode = (String) a.get("letter_code");
+    		String phoneNumber = (String) a.get("phone_number");
+    		MessageUtil message = new MessageUtil();
+    		message.sendSms(phoneNumber, letterCode);
+    	});
+    		
     	
     	
     	
