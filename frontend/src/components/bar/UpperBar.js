46747e5793eb9a8c46f3cf10c237a0764ef3558a
anseik <kma9271@naver.com>
2021-05-18 19:28:17 +0900

    feat: add alarm

--- frontend/src/components/bar/UpperBar.js
+++ frontend/src/components/bar/UpperBar.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import Badge from "@material-ui/core/Badge";
+import { useHistory ,useLocation, Link } from "react-router-dom";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import NotificationsIcon from "@material-ui/icons/Notifications";
-import Button from "@material-ui/core/Button";
-import { useHistory } from "react-router-dom";
-import "./UpperBar.css";
-import { TOKEN } from "../../constants";
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
+import { BASE_URL, TOKEN } from "../../constants";
 import sound1 from "pages/sounds/음악1.mp3";
-
 import ReactHowler from "react-howler";
+import axios from 'axios'
+import "./UpperBar.css";
+import {
+  Container,
+  AppBar,
+  Toolbar,
+  Grid,
+  Button,
+  Badge,
+  Typography,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@material-ui/core'
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -25,8 +41,9 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function UpperBar() {
-  const classes = useStyles();
-  const history = useHistory();
+  const classes = useStyles()
+  const history = useHistory()
+  const location = useLocation()
 
   const updateScroll = () => {
     setBarStyle(window.scrollY);
@@ -47,6 +64,63 @@ export default function UpperBar() {
   //   setBarStyle(null);
   // }
 
+  console.log(barStyle)
+
+
+  // 알림
+  const [alarmList, setAlarmList] = useState([
+    {
+      letterCode: 'L5l6P6N4a5',
+      title: 'title1',
+      userName: '안세익'
+    },
+    {
+      letterCode: 'L5l6P6N4a5',
+      title: 'title1',
+      userName: '안세익'
+    },
+    {
+      letterCode: 'L5l6P6N4a5',
+      title: 'title1',
+      userName: '안세익'
+    }
+  ])
+
+  const [alarmOpen, setAlarmOpen] = useState(false)
+
+  const alarmAxios = () => {
+    axios.get(BASE_URL + 'alarm/letters', {
+      headers: {
+        Authorization: TOKEN
+      }
+    })
+    .then(res => {
+      console.log(res)
+      setAlarmList(res.data)
+      // setAlarmOpen(!alarmOpen)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const handleDialogClose = () => {
+    setAlarmOpen(false);
+  };
+
+  const handleDialogOpen = () => {
+    setAlarmOpen(true);
+    alarmAxios()
+  };
+
+  const handleClickAlarm = (alarm, e) => {
+    setAlarmOpen(false);
+    // console.log(location)
+    history.push('/letter/detail/' + alarm.letterCode)
+  }
+
+
+
   useEffect(() => {
     window.addEventListener("scroll", updateScroll);
   });
@@ -57,61 +131,130 @@ export default function UpperBar() {
     }
   }, []);
 
-  console.log(barStyle)
+  useEffect(() => {
+    alarmAxios()
+  }, [])
+
+
   // 로그인한 유저일 경우 보여줄 상단바
   const userBar = (
-    <div>
-      <IconButton color="inherit" aria-label="notification" onClick={() => alert("알림")}>
-        <Badge badgeContent={10} color="secondary">
-          <NotificationsIcon />
-        </Badge>
-      </IconButton>
-      <IconButton color="inherit" aria-label="mypage" onClick={() => history.push("/mypage")}>
-        <AccountCircle />
-      </IconButton>
-      <Button
-        color="inherit"
-        onClick={() => {
-          setIsLogin(false);
-          window.localStorage.clear();
-          window.location.replace("/");
-        }}
-        style={{ fontSize: "18px" }}
-      >
-        로그아웃
-      </Button>
-    </div>
+    <Grid item xs={6}>
+    {/* 알람 버튼 */}
+      <Grid container>
+        <Grid item xs={3}>
+          <IconButton color="inherit" aria-label="notification" onClick={handleDialogOpen}>
+            <Badge badgeContent={alarmList.length} color="secondary">
+              <NotificationsIcon />
+            </Badge>
+          </IconButton>
+
+          <Dialog
+            open={alarmOpen}
+            onClose={handleDialogClose}
+            aria-labelledby="alarm-dialog-title"
+            aria-describedby="alarm-dialog-description"
+          >
+            <DialogTitle id="alarm-dialog-title">알림 조회</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alarm-dialog-description">
+                알림 온 레터 목록
+              </DialogContentText>
+              <div>
+                {alarmList.length > 0
+                  ? <List>
+                      {alarmList.map((alarm, index) => {
+                        return (
+                          // <Link to={'letter/detail/' + alarm.letterCode}>
+                            <ListItem key={index} alignItems="flex-start" onClick={(e) => handleClickAlarm(alarm, e)}>
+                              <ListItemIcon>
+                                <MailOutlineOutlinedIcon />
+                              </ListItemIcon>
+                              <ListItemText primary={alarm.title} secondary={'From. ' + alarm.name} />
+                            </ListItem>
+                          // </Link>
+                        )
+                      })}
+                    </List>
+                  : <Typography>확인하지 않은 레터가 없습니다.</Typography>
+                }
+              </div>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleDialogClose} color="primary" autoFocus>
+                닫기
+              </Button>
+            </DialogActions>
+          </Dialog>
+        </Grid>
+        
+        {/* 마이페이지 버튼 */}
+        <Grid item xs={3}>
+          <IconButton color="inherit" aria-label="mypage" onClick={() => history.push("/mypage")}>
+            <AccountCircle />
+          </IconButton>
+        </Grid>
+        
+        {/* 로그아웃 */}
+        <Grid item xs={6}>
+          <Button
+            color="inherit"
+            onClick={() => {
+              setIsLogin(false);
+              window.localStorage.clear();
+              window.location.replace("/");
+            }}
+            style={{ fontSize: "18px" }}
+          >
+            로그아웃
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 
   // 로그인하지 않은 게스트일 경우 보여줄 상단바
   const guestBar = (
-    <div>
-      <Button color="inherit" onClick={() => history.push("/login")} style={{ fontSize: "18px" }}>
-        로그인
-      </Button>
-    </div>
+    <Grid item xs={6}>
+      <Grid container>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}></Grid>
+        <Grid item xs={4}>
+          <Button color="inherit" onClick={() => history.push("/login")} style={{ fontSize: "18px", marginLeft: 'auto !important' }}>
+            로그인
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 
   return (
+    <Container maxWidth="xs">
     <AppBar position="fixed" className={barStyle > 1 ? "bgnav" : null}>
       <Toolbar>
-        <Typography className={classes.title} variant="h6" noWrap onClick={() => history.push("/")}>
-          TimeLetter
-        </Typography>
-        <div>
-          <ReactHowler src={sound1} playing={isPlay} />
-          {isPlay ? (
-            <div onClick={() => setIsPlay(false)} style={{ fontSize: "18px" }}>
-              "감성OFF"
+        <Grid container style={{alignItems: 'center'}}>
+          <Grid item xs={3}>
+            <Typography className={classes.title} variant="h6" noWrap onClick={() => history.push("/")}>
+              TimeLetter
+            </Typography>
+          </Grid>
+          <Grid item xs={3} style={{paddingLeft: '10px'}}>
+            <div>
+              <ReactHowler src={sound1} playing={isPlay} />
+              {isPlay ? (
+                <div onClick={() => setIsPlay(false)} style={{ fontSize: "18px" }}>
+                  "감성OFF"
+                </div>
+              ) : (
+                <div onClick={() => setIsPlay(true)} style={{ fontSize: "18px" }}>
+                  "감성ON"
+                </div>
+              )}
             </div>
-          ) : (
-            <div onClick={() => setIsPlay(true)} style={{ fontSize: "18px" }}>
-              "감성ON"
-            </div>
-          )}
-        </div>
-        <div>{isLogin ? userBar : guestBar}</div>
+          </Grid>
+          {isLogin ? userBar : guestBar}
+        </Grid>
       </Toolbar>
     </AppBar>
+    </Container>
   );
 }
