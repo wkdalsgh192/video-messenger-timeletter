4fe08dba40c9d27d82c73db7e528af0322595479
anseik <kma9271@naver.com>
2021-05-19 16:46:50 +0900

    feat : os detect

--- frontend/src/pages/TimeLetter/LetterCreate.js
+++ frontend/src/pages/TimeLetter/LetterCreate.js
@@ -47,28 +47,6 @@ import moment from 'moment';
 import './css/lettercreate.css'
 
 
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-      // Windows Phone must come first because its UA also contains "Android"
-    if (/windows phone/i.test(userAgent)) {
-        return "Windows Phone";
-    }
-
-    if (/android/i.test(userAgent)) {
-        return "Android";
-    }
-
-    // iOS detection from: http://stackoverflow.com/a/9039885/177710
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        return "iOS";
-    }
-
-    return "unknown";
-}
-
-console.log(getMobileOperatingSystem())
-
 // 테마
 const theme = createMuiTheme({
   // palette: {
@@ -468,6 +446,29 @@ const LetterCreate = () => {
     setVideoOpen(false)
   }
 
+  // detect mobile device os
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  
+        // Windows Phone must come first because its UA also contains "Android"
+      if (/windows phone/i.test(userAgent)) {
+          return "Windows Phone";
+      }
+  
+      if (/android/i.test(userAgent)) {
+          return "Android";
+      }
+  
+      // iOS detection from: http://stackoverflow.com/a/9039885/177710
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+          return "iOS";
+      }
+  
+      return "unknown";
+  }
+  
+  // console.log(getMobileOperatingSystem())
+
   
 
   // ************** return ****************
@@ -476,6 +477,7 @@ const LetterCreate = () => {
       <Typography className={classes.title} variant="h6">레터생성</Typography>
       {/* 캡슐 정보 */}
       <div className={classes.paper}>
+        <Typography>OS : {getMobileOperatingSystem()}</Typography>
         <form className={classes.form} noValidate onSubmit={onSubmit}>
           <Grid container direction="column">
 
